186b01bc2bacaa27c9ee7ea1e1d9eb11570de984
richardgu <richardgu@tencent.com>
2023-05-16 20:58:51 +0800

    build.sh

--- build.sh
+++ build.sh
@@ -2,6 +2,7 @@
 
 # build
 CGO_ENABLED=0 GOOS=linux GOARCH=amd64 go build -a -o artifacts/simple-image/kube-sample-apiserver
+echo "build kube-sample-apiserver end"
 
 # build image
 hubAddr="gymai/kube-sample-apiserver"
